5f2b8df59227b56a0e256f13365ce7c9a832da5b
Charles <charles_dahl@hotmail.com>
2026-05-29 03:10:01 +1000

    changed pyromill outputs from 3 to 2

--- prototypes/entities/pyromill.lua
+++ prototypes/entities/pyromill.lua
@@ -33,7 +33,7 @@ data:extend({
     collision_box = { { -0.7, -0.7 }, { 0.7, 0.7 } },
     selection_box = { { -0.8, -1 }, { 0.8, 1 } },
     crafting_categories = { "nullarbor-pyromilling" },
-    result_inventory_size = 3,
+    result_inventory_size = 2,
     energy_usage = "90kW",
     crafting_speed = 1,
     source_inventory_size = 1,
